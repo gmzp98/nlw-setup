456d94fe90ef12ff9c99d2e62739b05bb496495b
gmzp98 <daniel.gp98@hotmail.com>
2023-01-22 22:20:14 -0300

    Script update

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const button = document.querySelector('header button')
+const button = document.querySelector("header button")
 
-button.addEventListener('click', add)
-form.addEventListener('change', save)
+button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
-  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
-  if(dayExists) {
-    alert('Este dia já existe')
+  if (dayExists) {
+    alert("Opa!!❌ Você já adicionou o dia de hoje, volte amanhã.😉")
     return
   }
 
   nlwSetup.addDay(today)
-  alert('Pronto! Dia adicionado.')
+  alert("Prontinho! Dia adicionado.✔😁")
 }
 
 function save() {
   localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
